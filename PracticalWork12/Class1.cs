4c0395329663bcc6e1b62d1a38152b3ad4641d63
ИСП-31 <dunaevam2005@mail.ru>
2022-12-01 14:39:23 +0300

    дальше

---
+++ PracticalWork12/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork12
+{
+    internal class Class1
+    {
+        public static int SegmentLengthMultiplication(int point1, int point2, int point3)
+        {
+            int ac, bc;
+
+            ac = Math.Abs(point3 - point1);
+            bc = Math.Abs(point2 - point3);
+
+            return ac * bc;
+        }
+
+        public static void DigitSumMult(int num, out int sumCount, out int multCount)
+        {
+            int one = num / 100, two = num % 100 / 10, three = num % 10;
+            sumCount = one + two + three;
+            multCount = one * two * three;
+        }
+    }
+}
--- PracticalWork12/MainWindow.xaml.cs
+++ PracticalWork12/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PracticalWork12
 {
@@ -32,32 +33,20 @@ namespace PracticalWork12
                 int point1 = Convert.ToInt32(a.Text);
                 int point2 = Convert.ToInt32(b.Text);
                 int point3 = Convert.ToInt32(c.Text);
-                int ac, bc, dupl;
 
-                if (point3 < point1)
+                if (point3 < point1 && point3 > point2 || point3 > point1 && point3 < point2)
                 {
-                    dupl = point3;
-                    point3 = point1;
-                    point1 = dupl;
-                    a.Text = point1.ToString();
-                    c.Text = point3.ToString();
+                    int res = Class1.SegmentLengthMultiplication(point1, point2, point3);
+                    result.Text = res.ToString();
+
+                    a.Focus();
                 }
-                else if (point3 > point2)
+                else
                 {
-                    dupl = point3;
-                    point3 = point2;
-                    point2 = dupl;
-                    a.Text = point2.ToString();
-                    c.Text = point3.ToString();
+                    MessageBox.Show("Точка C должна быть между точками A и B");
                 }
-
-                ac = point3 - point1;
-                bc = point2 - point3;
-                result.Text = Convert.ToString(ac * bc);
-
-                a.Focus();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
@@ -70,7 +59,7 @@ namespace PracticalWork12
 
         private void btn_Exit_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void btn_About_Click(object sender, RoutedEventArgs e)
@@ -79,6 +68,58 @@ namespace PracticalWork12
                 "Даны три точки A, B, C на числовой оси." +
                 " Точка C расположена между точками A и " +
                 "B. Найти произведение длин отрезков AC и BC.");
+        }
+
+        DispatcherTimer timer;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            datetime.Text = d.ToString("HH:mm dd.MM.yy");
+        }
+
+        private void btn_Count2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int.TryParse(number.Text, out int num);
+
+                if (num / 1000 < 1)
+                {
+
+                    Class1.DigitSumMult(num, out int sumCount, out int multCount);
+                    mult.Text = multCount.ToString();
+                    sum.Text = sumCount.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Введите трёхзначное число");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_About2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №12 \n\nДунаева М.И.\n\n" +
+                " Дано трехзначное число. Найти сумму и произведение его цифр");
+        }
+
+        private void Clear1_Click(object sender, RoutedEventArgs e)
+        {
+            a.Clear();
+            b.Clear();
+            c.Clear();
         }
     }
 }
